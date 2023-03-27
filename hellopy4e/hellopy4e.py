408d216865ab6d2514831d0734e942ceb34bec99
callmeminji <giyomkoala@gmail.com>
2023-03-28 03:08:30 +0900

    first

---
+++ hellopy4e/hellopy4e.py
@@ -0,0 +1,32 @@
+"""
+Written by M.J.Hong
+
+starting date 23.03.27
+last update 23.03.27 
+"""
+
+print("Love Ya!")
+
+
+x=2
+print(x)
+x=x+3
+print(x)    #sequence
+
+x=5
+if x<10:
+    print('smaller')
+if x>10:
+    print('bigger')
+print('Fin')    #conditional
+
+n=5
+while n>0:
+    print(n)
+    n=n-1
+print('Blast Off!')     #repeated
+
+
+
+
+
